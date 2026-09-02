5b9e04d96a8b4522eabc7c053e0a9684544fb342
LUÍS MIGUEL PEREIRA LIMA <58069799897@info.local>
2026-09-02 09:16:03 -0300

    trataivas de registro ponto concluidas :rocket:

--- ServSegFacilitiesAPI/ServSegFacilitiesAPI/Program.cs
+++ ServSegFacilitiesAPI/ServSegFacilitiesAPI/Program.cs
@@ -106,8 +106,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
